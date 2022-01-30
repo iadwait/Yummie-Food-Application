c9a3a8f27e68a03900e95c5b116dbaf322024301
Adwait Barkale <adwaitb@winjit.com>
2022-01-30 16:05:13 +0530

    Started Working on Home Page

---
+++ Yummie Food App/Views/Home/Controller/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  Yummie Food App
+//
+//  Created by Adwait Barkale on 30/01/22.
+//
+
+import UIKit
+
+/// This Class is for Home View Controller
+class HomeViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    // MARK: - Variable Declarations
+    
+    // MARK: - View's Initialization Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Yummie"
+    }
+    
+    // MARK: - User Defined Functions
+    
+    // MARK: - IBActions
+
+}
